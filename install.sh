f352aba883024499386715ab130cb72d3fe50532
Ryan Cassidy <ryan.cassidy@boston.gov>
2015-07-20 17:31:37 -0400

    Better install script.

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-ln -s ./tmux.conf ~/.tmux.conf
-ln -s ./vimrc ~/.vimrc
-ln -s ./zshrc ~/.zshrc
+TMUX_LOC=$(readlink -f tmux.conf)
+VIM_LOC=$(readlink -f vimrc)
+ZSH_LOC=$(readlink -f zshrc)
+
+ln -s $TMUX_LOC ~/.tmux.conf
+ln -s $VIM_LOC ~/.vimrc
+ln -s $ZSH_LOC ~/.zshrc
